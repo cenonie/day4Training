421896a4fbe16969b813dcd9805d52a3bf19f196
PLANACE <PLANACE@ZHA-ITA107-W7.corp.oocl.com>
2017-08-04 13:44:10 +0800

    branch 5-1.2

--- Day5Exercises/src/com/gitTry/git0804.java
+++ Day5Exercises/src/com/gitTry/git0804.java
@@ -6,5 +6,7 @@ public class git0804 {
 		System.out.println("start add 1+1 " + (1+1));
 		System.out.println("start add 2+2 " + (2+2));
 		System.out.println("start add 3+3 " + (3+3));
+		System.out.println("start add 4+4 " + (4+4));
+		System.out.println("start add 5+5 " + (5+5));
 	}
 }
