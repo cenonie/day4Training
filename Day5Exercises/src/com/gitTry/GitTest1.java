3237a5cb68a335746b5257bcb27cd0b0e0953c9a
PLANACE <PLANACE@ZHA-ITA107-W7.corp.oocl.com>
2017-08-04 11:15:52 +0800

    day 5 sample

---
+++ Day5Exercises/src/com/gitTry/GitTest1.java
@@ -0,0 +1,8 @@
+package com.gitTry;
+
+public class GitTest1 {
+	public static void main(String args[])
+	{
+		System.out.println("Git Test");
+	}
+}
--- MavenTest/src/test/java/com/cenonDay4/AppTest1.java
+++ MavenTest/src/test/java/com/cenonDay4/AppTest1.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AppTest1 {
-
+	
 	@Test
 	public void test() {
-		if(App1.printHello(10) == false)
+		if(App1.printHello(11) == false)
 			fail("test failed");
 	}
-
+	
 }
